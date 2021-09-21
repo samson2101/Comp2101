beb4dd427a8272ee216901cb7028ec5036a182b1
samson2101 <ayorinde.adeniyi@yahoo.com>
2021-09-20 21:27:32 -0400

    README.md

---
+++ helloworld.sh
@@ -0,0 +1,6 @@
+
+#!/bin/bash
+
+# My first script - helloworld.sh.
+
+echo 'hello world!'
---
+++ helloworldtemplated.sh
@@ -0,0 +1,10 @@
+
+#!/bin/bash
+
+# My second script - helloworldtemplated.sh
+
+cat <<E0F
+hello world
+I am process# $$
+E0F
+
